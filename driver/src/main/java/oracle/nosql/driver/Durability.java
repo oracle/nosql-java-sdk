442aa9430a08a65fb15ea8ece40c81a7649f927a
John Connelly <john.connelly@oracle.com>
2021-06-08 14:24:25 -0700

    Fixed copyright

--- driver/src/main/java/oracle/nosql/driver/Durability.java
+++ driver/src/main/java/oracle/nosql/driver/Durability.java
@@ -1,14 +1,8 @@
 /*-
- * Copyright (C) 2011, 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2021 Oracle and/or its affiliates. All rights reserved.
  *
- * This file was distributed by Oracle as part of a version of Oracle NoSQL
- * Database made available at:
- *
- * http://www.oracle.com/technetwork/database/database-technologies/nosqldb/downloads/index.html
- *
- * Please see the LICENSE file included in the top-level directory of the
- * appropriate version of Oracle NoSQL Database for a copy of the license and
- * additional information.
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ *  https://oss.oracle.com/licenses/upl/
  */
 
 package oracle.nosql.driver;
