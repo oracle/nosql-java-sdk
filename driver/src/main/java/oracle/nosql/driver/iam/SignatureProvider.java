8bb3d48cc818d7e425e892b6dcf8e2fd3dc7401a
George Feinberg <george.feinberg@oracle.com>
2023-07-24 17:12:23 -0400

    Updated javadoc

--- driver/src/main/java/oracle/nosql/driver/iam/SignatureProvider.java
+++ driver/src/main/java/oracle/nosql/driver/iam/SignatureProvider.java
@@ -650,16 +650,20 @@ public class SignatureProvider
     /**
      * Creates a SignatureProvider using a temporary session token read from
      * a token file. The path of token file is read from the default profile
-     * in configuration file at the default location. The configuration file
-     * used is <code>~/.oci/config</code>. See
+     * in configuration file at the default location, the value of field
+     * <code>security_token_file</code>. The configuration file used is
+     * <code>~/.oci/config</code>. See
      * <a href="https://docs.cloud.oracle.com/iaas/Content/API/Concepts/sdkconfig.htm">SDK Configuration File</a>
      * for details of the file's contents and format.
-     *
+     * <p>
+     * See <a href="https://docs.oracle.com/en-us/iaas/Content/API/Concepts/sdk_authentication_methods.htm#sdk_authentication_methods_session_token">Session Token-Based Authentication</a>
+     * for more details of session-token-based authentication.
+     * <p>
+     * You can use the OCI CLI to authenticate and create a token, see
+     * See <a href="https://docs.oracle.com/en-us/iaas/Content/API/SDKDocs/clitoken.htm">Token-based Authentication for the CLI</a>.
      * <p>
      * When using this constructor the user has a default compartment for
      * all tables. It is the root compartment of the user's tenancy.
-     * <p>
-     * See <a href="https://docs.oracle.com/en-us/iaas/Content/API/SDKDocs/clitoken.htm">Token-based Authentication for the CLI</a>.
      *
      * @return SignatureProvider
      */
@@ -672,16 +676,20 @@ public class SignatureProvider
     /**
      * Creates a SignatureProvider using a temporary session token read from
      * a token file. The path of token file is read from the specified profile
-     * in configuration file at the default location. The configuration file
-     * used is <code>~/.oci/config</code>. See
+     * in configuration file at the default location, the value of field
+     * <code>security_token_file</code>. The configuration file used is
+     * <code>~/.oci/config</code>. See
      * <a href="https://docs.cloud.oracle.com/iaas/Content/API/Concepts/sdkconfig.htm">SDK Configuration File</a>
      * for details of the file's contents and format.
-     *
+     * <p>
+     * See <a href="https://docs.oracle.com/en-us/iaas/Content/API/Concepts/sdk_authentication_methods.htm#sdk_authentication_methods_session_token">Session Token-Based Authentication</a>
+     * for more details of session-token-based authentication.
+     * <p>
+     * You can use the OCI CLI to authenticate and create a token, see
+     * <a href="https://docs.oracle.com/en-us/iaas/Content/API/SDKDocs/clitoken.htm">Token-based Authentication for the CLI</a>.
      * <p>
      * When using this constructor the user has a default compartment for
      * all tables. It is the root compartment of the user's tenancy.
-     * <p>
-     * See <a href="https://docs.oracle.com/en-us/iaas/Content/API/SDKDocs/clitoken.htm">Token-based Authentication for the CLI</a>.
      *
      * @param profile profile name used to load session token
      *
@@ -696,16 +704,20 @@ public class SignatureProvider
     /**
      * Creates a SignatureProvider using a temporary session token read from
      * a token file. The path of token file is read from the specified profile
-     * in configuration file at the specified location. The configuration file
+     * in configuration file at the specified location, the value of field
+     * <code>security_token_file</code>. The configuration file
      * used is <code>~/.oci/config</code>. See
      * <a href="https://docs.cloud.oracle.com/iaas/Content/API/Concepts/sdkconfig.htm">SDK Configuration File</a>
      * for details of the file's contents and format.
-     *
+     * <p>
+     * See <a href="https://docs.oracle.com/en-us/iaas/Content/API/Concepts/sdk_authentication_methods.htm#sdk_authentication_methods_session_token">Session Token-Based Authentication</a>
+     * for more details of session-token-based authentication.
+     * <p>
+     * You can use the OCI CLI to authenticate and create a token, see
+     * <a href="https://docs.oracle.com/en-us/iaas/Content/API/SDKDocs/clitoken.htm">Token-based Authentication for the CLI</a>.
      * <p>
      * When using this constructor the user has a default compartment for
      * all tables. It is the root compartment of the user's tenancy.
-     * <p>
-     * See <a href="https://docs.oracle.com/en-us/iaas/Content/API/SDKDocs/clitoken.htm">Token-based Authentication for the CLI</a>.
      *
      * @param configFilePath path of configuration file
      *
