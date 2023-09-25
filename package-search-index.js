72772a22e599c3dddc9fc243e844fc30169abf88
John Connelly <john.connelly@oracle.com>
2023-09-25 13:43:26 -0700

    Update Javadoc for release

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"oracle.nosql.driver"},{"l":"oracle.nosql.driver.iam"},{"l":"oracle.nosql.driver.kv"},{"l":"oracle.nosql.driver.ops"},{"l":"oracle.nosql.driver.ops.serde"},{"l":"oracle.nosql.driver.values"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"oracle.nosql.driver"},{"l":"oracle.nosql.driver.iam"},{"l":"oracle.nosql.driver.kv"},{"l":"oracle.nosql.driver.ops"},{"l":"oracle.nosql.driver.values"}];updateSearchResults();
