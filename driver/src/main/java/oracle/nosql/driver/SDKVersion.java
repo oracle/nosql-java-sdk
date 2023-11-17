effdeac79f2ec168b44ff685e7d26914fc85c241
John Connelly <john.connelly@oracle.com>
2023-11-17 12:19:30 -0800

    Preparing for 5.4.14 release

--- driver/src/main/java/oracle/nosql/driver/SDKVersion.java
+++ driver/src/main/java/oracle/nosql/driver/SDKVersion.java
@@ -12,5 +12,5 @@ public class SDKVersion {
     /**
      * The full X.Y.Z version of the current SDK
      */
-    public static final String VERSION = "5.4.13";
+    public static final String VERSION = "5.4.14";
 }
