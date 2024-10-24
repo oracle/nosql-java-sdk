fd7f48be60158b70f2945d66e7bc7b5c37eb9eac
markoszah <74737089+markoszah@users.noreply.github.com>
2024-10-24 18:50:32 +0300

    added PrepareQueryException

---
+++ driver/src/main/java/oracle/nosql/driver/PrepareQueryException.java
@@ -0,0 +1,25 @@
+/*-
+ * Copyright (c) 2011, 2024 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ *  https://oss.oracle.com/licenses/upl/
+ */
+
+package oracle.nosql.driver;
+
+/**
+ * The query must be recompiled after DDL changes on the tables and/or
+ * indexes that are accessed by the query.
+ */
+public class PrepareQueryException extends NoSQLException {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @hidden
+     * @param msg the exception message
+     */
+    public PrepareQueryException(String msg) {
+        super(msg);
+    }
+}
