a4e8219a2e4777a9a1463bf06e9b3993ce3c9ff4
markoszah <74737089+markoszah@users.noreply.github.com>
2025-01-08 19:05:18 +0200

    updated decrementQueryVersion() method

--- driver/src/main/java/oracle/nosql/driver/http/Client.java
+++ driver/src/main/java/oracle/nosql/driver/http/Client.java
@@ -631,7 +631,7 @@ public class Client {
                  */
                 kvRequest.setTimeoutInternal(thisIterationTimeoutMs);
                 serialVersionUsed = writeContent(buffer, kvRequest,
-                    queryVersionUsed);
+                                                 queryVersionUsed);
                 kvRequest.setTimeoutInternal(timeoutMs);
 
                 /*
@@ -1624,14 +1624,17 @@ public class Client {
      *         false: already at lowest version number.
      */
     private synchronized boolean decrementQueryVersion(short versionUsed) {
+
         if (queryVersion != versionUsed) {
             return true;
         }
-        if (queryVersion == QueryDriver.QUERY_V4) {
-            queryVersion = QueryDriver.QUERY_V3;
-            return true;
+
+        if (queryVersion == QueryDriver.QUERY_V3) {
+            return false;
         }
-        return false;
+
+        --queryVersion;
+        return true;
     }
 
     /**
